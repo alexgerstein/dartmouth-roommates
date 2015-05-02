8a2aceb99ef1b086a0cfcb9f956525ad7f422e5c
Alex Gerstein <gerstein.alex@gmail.com>
2015-05-02 16:39:25 -0400

    Add frontend templates.

--- dartmates/api/users.py
+++ dartmates/api/users.py
@@ -1,7 +1,6 @@
 from flask.ext.restful import Resource
-from flask.ext.login import current_user, logout_user
+from flask.ext.login import login_required, current_user, logout_user
 
-from dartmates.login import login_required
 from dartmates.database import db
 
 
