34cff73fe7c7b1561e02cb8142e946348bfead8c
Alex Gerstein <gerstein.alex@gmail.com>
2015-05-02 22:17:50 -0400

    Remove warning log limit.

--- config/default.py
+++ config/default.py
@@ -10,8 +10,6 @@ SECRET_KEY = 'youll-never-guess'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_RECORD_QUERIES = True
 
-logging.basicConfig(level=logging.WARNING)
-
 logger = logging.getLogger('DARTmates')
 logger.setLevel(logging.INFO)
 
