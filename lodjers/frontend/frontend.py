1086a5606e3ddb97189ce484f0d87f174c515919
Alex Gerstein <gerstein.alex@gmail.com>
2015-05-07 19:04:10 -0400

    Fix login view.

--- lodjers/frontend/frontend.py
+++ lodjers/frontend/frontend.py
@@ -1,7 +1,6 @@
 from . import bp
 from flask import render_template
-from flask.ext.security import login_required
-from lodjers.models import User
+from flask.ext.login import login_required
 
 
 @bp.route('/')
